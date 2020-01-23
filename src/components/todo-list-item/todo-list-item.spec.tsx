cd4e806759ef5f7fb1694033481bb748ca0e1645
Anya Efimova <gani4ka_n@ukr.net>
2020-01-23 22:13:03 +0200

    test test

---
+++ src/components/todo-list-item/todo-list-item.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TodoListItem, TodoListItemProps } from './todo-list-item';
+
+describe('<TodoListItem />', (): void => {
+  it('should render correctly', (): void => {
+    const props: TodoListItemProps = {
+      label: 'text',
+      onDeleted: jest.fn(),
+      onToggleImportant: jest.fn(),
+      onToggleDone: jest.fn(),
+      important: false,
+      done: false
+    };
+    const component = shallow(<TodoListItem {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
