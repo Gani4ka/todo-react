0916f34ff5b06018d505ff60e6372de63067a5d7
Anya Efimova <anna.efimova@gen.tech>
2020-01-24 11:59:29 +0200

    fixed test env

--- jest.config.js
+++ jest.config.js
@@ -1,52 +1,10 @@
 module.exports = {
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  clearMocks: true,
-  moduleDirectories: ['node_modules'],
-  globals: {
-    'ts-jest': {
-      tsConfig: 'jest.tsconfig.json'
-    }
-  },
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'setup-enzyme.ts'
-    // '*.stub.ts',
-    // '*.d.ts'
-  ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFilesAfterEnv: ['<rootDir>/setup-enzyme.ts'],
-  testEnvironment: 'jsdom',
-  timers: 'fake',
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/file-mock.js',
-    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styles-mock.js',
-    '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@mocks/(.*)$': '<rootDir>/__mocks__/$1'
-  },
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: 'jest tests',
-        outputDirectory: './coverage',
-        outputName: 'junit.xml'
-      }
-    ]
-  ],
-  coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  }
+  setupTestFrameworkScriptFile: '<rootDir>/src/setupEnzyme.ts',
 };
